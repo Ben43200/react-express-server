6049be09c1d391b545a13bde59845d9374ba6b5a
meteoben <meteoben@gmail.com>
2024-01-19 19:26:16 +0100

    ajout dotenv

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express()
+// const dotenv = require('dotenv')
+require('dotenv').config()
 
-
+const port = process.env.PORT || 5000
 
 app.get("/api", (req,res) => {
     res.json({"users": ["userOne", "userTwo", "userThree", "userFour"]})
 })
 
 
-app.listen(5000, "0.0.0.0", () => { console.log("server started on port 5000")})
+app.listen(port, "0.0.0.0", () => { console.log("server started on port 5000")})
