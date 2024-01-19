a95eebc06f074db84191e70838cc515282049ab8
meteoben <meteoben@gmail.com>
2024-01-19 18:37:30 +0100

    ajout user 4

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 
 
 app.get("/api", (req,res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
+    res.json({"users": ["userOne", "userTwo", "userThree", "userFour"]})
 })
 
 
